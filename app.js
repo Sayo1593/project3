aa9892256537d634c96af7748edc45c3e4b06271
javastudyreview <rhwjdtlscjd123@gmail.com>
2024-05-21 16:41:16 +0900

    feature: New Calculator code

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
+
 const firstValue = document.getElementById('first');
 const secondValue = document.getElementById('second');
-
 const submitForm = document.getElementById('form');
 const output = document.getElementById('output');
 
-document.getElementById("sub").addEventListener("click",sub);
-
-
-//곱셈 결과
-const multi_result = document.getElementById('multi_result');
-// 결과값 출력 핸들러
-const formSubmitHandler = (event) => {
-  event.preventDefault();
-
-  // 덧셈 함수 호출
-  const result = add(Number(firstValue.value), Number(secondValue.value));
-
-  // 결과 값 출력
-  output.textContent = result;
-};
-
-submitForm.addEventListener('submit', formSubmitHandler);
-
 // 덧셈 함수
-const add = (a, b) => a + b;
+document.getElementById('add').addEventListener('click', () => {
+  const result = add(Number(firstValue.value), Number(secondValue.value));
+  output.textContent = `${result}`;
+});
 
 // 뺄셈 함수
-function sub() {
-  const result = (Number(firstValue.value))-(Number(secondValue.value));
-  output.textContent = result;
-}
-//곱셈 함수
-const mult = function(a,b){
-  const result = Number(firstValue.value) * Number(secondValue.value);
-  multi_result.textContent = `${Number(firstValue.value)} x ${Number(secondValue.value)} = ${result}`;
-}
-document.getElementById("multi").onclick = mult;
+document.getElementById('sub').addEventListener('click', () => {
+  const result = subtract(Number(firstValue.value), Number(secondValue.value));
+  output.textContent = `${result}`;
+});
+
+// 곱셈 함수
+document.getElementById('mult').addEventListener('click', () => {
+  const result = multiply(Number(firstValue.value), Number(secondValue.value));
+  output.textContent = `${result}`;
+});
+
+// 나눗셈 함수
+document.getElementById('div').addEventListener('click', () => {
+  const result = divide(Number(firstValue.value), Number(secondValue.value));
+  output.textContent = `${result}`;
+});
+
+// 덧셈
+const add = (a, b) => a + b;
+
+// 뺄셈
+const subtract = (a, b) => a - b;
+
+// 곱셈
+const multiply = (a, b) => a * b;
+
+// 나눗셈
+const divide = (a, b) => {
+  if (b === 0) {
+    return "0으로 나눌 수 없습니다.";
+  }
+  return a / b;
+};
